9a8393c31e95e24fd49d51ba08579891ee7755a4
Tat Nguyen Van <nguyenvantat1182002@gmail.com>
2024-02-04 21:54:08 +0700

    update

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def main(lock: threading.Lock, tinproxy: TinProxy, chrome_pos: tuple, fp_gen: Fi
                 victim = marketfruitrb(proxy)
                 victim.driver.set.window.size(CHROME_WIDTH, CHROME_HEIGHT)
                 victim.driver.set.window.location(*chrome_pos)
-            # victim = marketfruitrb('anhphi123_224:9LVv9WWFen_country-vn@geo.vinacloud.vn:11222', fp_gen)
+            # victim = xamroboxvn('anhphi123_224:9LVv9WWFen_country-vn@geo.vinacloud.vn:11222', fp_gen)
             victim.run()
         except ProxyError as e:
             print(e)
@@ -37,10 +37,11 @@ def main(lock: threading.Lock, tinproxy: TinProxy, chrome_pos: tuple, fp_gen: Fi
         except Exception as e:
             print(type(e).__name__)
         finally:
-            try:
-                victim.close()
-            except Exception:
-                pass
+            with lock:
+                try:
+                    victim.close()
+                except Exception:
+                    pass
 
 
 with open('config.json', encoding='utf-8') as file:
--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-# from victims import *
-# from chrome_fingerprints import FingerprintGenerator
+from victims import *
+from chrome_fingerprints import FingerprintGenerator
 
 
-# fp_gen = FingerprintGenerator()
-# victim = hiddingroblox(None, fp_gen)
-# victim.check2('hiddingroblox.txt', victim.get_items)
+fp_gen = FingerprintGenerator()
+victim = robuxstorevn(None, fp_gen)
+victim.check2('robuxstorevn.txt', victim.get_items)
 # victim.run()
 
 # import capsolver
@@ -14,22 +14,22 @@
 # victim = shoptridung('brZgipEN:OvBNcLMk@171.235.160.120:21001', fp_gen)
 # victim.run()
 
-from DrissionPage import ChromiumPage, ChromiumOptions
+# from DrissionPage import ChromiumPage, ChromiumOptions
 
 
-import time
+# import time
 
 
-options = ChromiumOptions().auto_port(tmp_path='profiles')
-options.set_proxy('http://127.0.0.1:4000')
-driver = ChromiumPage(addr_or_opts=options)
-try:
-    driver.get('https://www.facebook.com/', show_errmsg=True, retry=1)
-    time.sleep(4)
-except Exception as e:
-    print(type(e).__name__)
+# options = ChromiumOptions().auto_port(tmp_path='profiles')
+# options.set_proxy('http://127.0.0.1:4000')
+# driver = ChromiumPage(addr_or_opts=options)
+# try:
+#     driver.get('https://www.facebook.com/', show_errmsg=True, retry=1)
+#     time.sleep(4)
+# except Exception as e:
+#     print(type(e).__name__)
 
-driver.quit()
+# driver.quit()
 
 
 
